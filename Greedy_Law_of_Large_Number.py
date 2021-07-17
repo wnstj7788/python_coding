653ff72f15933d4ff9100d8da6614aa287ed59cd
이루이루 <61938768+wnstj7788@users.noreply.github.com>
2021-07-17 07:19:31 +0000

    Greedy 실전문제 1번

--- Greedy_Law_of_Large_Number.py
+++ Greedy_Law_of_Large_Number.py
@@ -1,15 +1,18 @@
 # 나눠서 입력 받기
 n,m,k = map(int, input().split())
 
+# 데이터를 리스트로 저장
 date= list(map(int,input().split()))
 
+# 오름차순 정렬
 date.sort()
 
+#가장 큰 수 2개를 추림 
 one_big = date[n-1]
 two_bing = date[n-2]
-
+#
 sum = 0
-
+# m에 맞게 더한 후 2번 쨰로 큰 값을 더해줌 
 while True:
   for i in range(k):
     if m == 0 :
